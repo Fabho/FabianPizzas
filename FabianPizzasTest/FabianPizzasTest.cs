cefbb3ff7ff76f2fd265a507ee14f17c2d02df8a
Fabho <fabian.calsina@gmail.com>
2020-05-28 20:23:28 -0400

    Include back end project

---
+++ FabianPizzasTest/FabianPizzasTest.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using FabianPizzas.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FabianPizzasTest
+{
+    [TestClass]
+    public class FabianPizzasTest
+    {
+        //TODO: I must learn how to moq and test entity framework instances
+        
+        [TestMethod]
+        public void ToppingsModel_shouldBeValid()
+        {
+            Topping mockTopping = new Topping()
+            {
+                ToppingID = 1,
+                Name = "Mozzarella"
+            };
+
+            Assert.IsNotNull(mockTopping);
+            Assert.IsNotNull(mockTopping.ToppingID);
+        }
+
+        [TestMethod]
+        public void PizzaModel_shouldBeValid()
+        {
+            Pizza mockPizza = new Pizza
+            {
+                PizzaID = 1,
+                Name = "Supreme",
+                Toppings = new List<Topping>()
+                        {
+                            new Topping
+                            {
+                                ToppingID = 1,
+                                Name = "Mozzarella"
+                            },
+                            new Topping
+                            {
+                                ToppingID = 2,
+                                Name = "Bacon"
+                            }
+                        }
+            };
+
+
+            Assert.IsNotNull(mockPizza);
+            Assert.IsTrue(!String.IsNullOrEmpty(mockPizza.Name));
+            Assert.IsTrue(mockPizza.Toppings.Count() > 0);
+        }
+
+        [TestMethod]
+        public void MenuModel_shouldBeValid()
+        {
+            var mockMenu = new Menu
+            {
+                MenuID = 1,
+                Name = "Fabian Pizzas",
+                Pizzas = new List<Pizza>()
+                {
+                    new Pizza
+                    {
+                        PizzaID = 1,
+                        Name = "Supreme",
+                        Toppings = new List<Topping>()
+                        {
+                            new Topping
+                            {
+                                ToppingID = 1,
+                                Name = "Mozzarella"
+                            },
+                            new Topping
+                            {
+                                ToppingID = 2,
+                                Name = "Bacon"
+                            }
+                        }
+                    },
+                    new Pizza
+                    {
+                        PizzaID = 2,
+                        Name = "Hawaiian ",
+                        Toppings = new List<Topping>()
+                        {
+                            new Topping
+                            {
+                                ToppingID = 3,
+                                Name = "Ham"
+                            },
+                            new Topping
+                            {
+                                ToppingID = 4,
+                                Name = "pineapple"
+                            }
+                        }
+                    }
+                }
+            };
+
+            Assert.IsNotNull(mockMenu);
+            Assert.IsTrue(!String.IsNullOrEmpty(mockMenu.Name));
+            Assert.IsTrue(mockMenu.Pizzas.Count() > 0);
+        }
+    }
+}
---
+++ FabianPizzasTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FabianPizzasTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FabianPizzasTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3a9b9a4d-f11d-43d6-a7a5-5c74adcb0203")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
